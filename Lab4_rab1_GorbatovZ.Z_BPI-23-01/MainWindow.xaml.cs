94c7cfe86d52853cb41658a1642f69d03d4c6f64
ZAK <xoriartycode@gmail.com>
2025-10-13 12:02:55 +0500

    Создание View

--- Lab4_rab1_GorbatovZ.Z_BPI-23-01/MainWindow.xaml.cs
+++ Lab4_rab1_GorbatovZ.Z_BPI-23-01/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab4_rab1_GorbatovZ.Z_BPI_23_01.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,17 @@ namespace Lab4_rab1_GorbatovZ.Z_BPI_23_01
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Employee_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowEmployee wEmployee = new WindowEmployee();
+            wEmployee.Show();
+        }
+
+        private void Role_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowRole wRole = new WindowRole();
+            wRole.Show();
         }
     }
 }
--- Lab4_rab1_GorbatovZ.Z_BPI-23-01/View/WindowRole.xaml.cs
+++ Lab4_rab1_GorbatovZ.Z_BPI-23-01/View/WindowRole.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab4_rab1_GorbatovZ.Z_BPI_23_01.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Lab4_rab1_GorbatovZ.Z_BPI_23_01.View
         public WindowRole()
         {
             InitializeComponent();
+            DataContext = new RoleViewModel();
         }
     }
 }
